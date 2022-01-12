8733629d1ed1a6653f38e21b6e13b06a163e507c
realPlut0n1um <96692345+realPlut0n1um@users.noreply.github.com>
2022-01-12 19:57:16 +0900

    Update cyclops.cpp

--- cyclops.cpp
+++ cyclops.cpp
@@ -37,7 +37,8 @@ void cyclops::nsa0day(){
     	std::stringstream nsaKeys; // stream for our decrypted key
 	
 	std::cout << "[Cyclops_0day]: Performing Decryption" << std::endl;
-		
+	
+	// lmao, I half assed this, but whatever
 	for(int z = 0; z < 39; z++){
 		nsaDecryptedKeys[z] = ((((((nsaSecretKeys[z] << 0x02) / 4) << 0x04) / 8) * 0x20) >> 0x06);
 	}
